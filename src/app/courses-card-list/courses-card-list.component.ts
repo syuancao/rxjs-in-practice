2b035dd8a34c01bffab49cf245029f66f92fd975
syuancao <73942181+syuancao@users.noreply.github.com>
2022-05-21 15:02:38 +0800

    add courses-card-list component

---
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { Course } from '../model/course';
+import { MatDialogConfig } from '@angular/material/dialog';
+
+@Component({
+  selector: 'courses-card-list',
+  templateUrl: './courses-card-list.component.html',
+  styleUrls: ['./courses-card-list.component.scss'],
+})
+export class CoursesCardListComponent {
+  @Input()
+  courses: Course[] | undefined;
+
+  constructor() {}
+
+  editCourse(course: Course) {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+
+    dialogConfig.data = course;
+  }
+}
