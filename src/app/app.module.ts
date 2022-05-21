5acb95bb191e3821468be83fdc01986cc4ae9740
syuancao <73942181+syuancao@users.noreply.github.com>
2022-05-21 20:01:23 +0800

    import some modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,42 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { CourseComponent } from './course/course.component';
+import { CourseDialogComponent } from './course-dialog/course-dialog.component';
+import { CoursesCardListComponent } from './courses-card-list/courses-card-list.component';
+import { HomeComponent } from './home/home.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { RouterModule } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    CourseComponent,
+    CourseDialogComponent,
+    CoursesCardListComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    RouterModule,
+    MatTabsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
