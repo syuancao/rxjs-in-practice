7aebd2fbe2d8ec6bbf7881510d2ed7710181871a
syuancao <73942181+syuancao@users.noreply.github.com>
2022-05-24 23:01:43 +0800

    Form Draft Pre-Save Example and the RxJs Filter Operator

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,15 +1,16 @@
-import { Component, ElementRef, Inject, ViewChild } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Course } from '../model/type';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import * as moment from 'moment';
+import { filter, from } from 'rxjs';
 
 @Component({
   selector: 'course-dialog',
   templateUrl: './course-dialog.component.html',
   styleUrls: ['./course-dialog.component.scss'],
 })
-export class CourseDialogComponent {
+export class CourseDialogComponent implements OnInit {
   form: FormGroup;
   course: Course;
 
@@ -29,6 +30,22 @@ export class CourseDialogComponent {
       category: [course.category, Validators.required],
       releasedAt: [moment(), Validators.required],
       longDescription: [course.longDescription, Validators.required],
+    });
+  }
+
+  ngOnInit() {
+    this.form.valueChanges.pipe(filter(() => this.form.valid)).subscribe((changes) => {
+      const saveCourse$ = from(
+        fetch(`/api/courses/${this.course.id}`, {
+          method: 'PUT',
+          body: JSON.stringify(changes),
+          headers: {
+            'content-type': 'application/json',
+          },
+        }),
+      );
+
+      saveCourse$.subscribe();
     });
   }
 
