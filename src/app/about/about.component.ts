1258e2296b910bcd6f0837915f44dd9f35c8c359
syuancao <73942181+syuancao@users.noreply.github.com>
2022-05-28 23:51:29 +0800

    Understanding the merge Observable combination Strategy

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { concat, of } from 'rxjs';
+import { interval, map, mergeWith } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -10,14 +10,10 @@ export class AboutComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    const source1$ = of(1, 2, 3);
+    const interval1$ = interval(1000);
 
-    const source2$ = of(4, 5, 6);
+    const interval2$ = interval1$.pipe(map((val) => 10 * val));
 
-    const source3$ = of(7, 8, 9);
-
-    const result$ = concat(source1$, source2$, source3$);
-
-    result$.subscribe(console.log);
+    interval2$.pipe(mergeWith(interval1$)).subscribe(console.log);
   }
 }
