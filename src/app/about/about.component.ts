0baeb7f4722cfa761f20455823eafdeb73daa349
syuancao <73942181+syuancao@users.noreply.github.com>
2022-05-31 00:19:26 +0800

    BehaviorSubject In Detail - When to Use it and Why

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -10,15 +10,21 @@ export class AboutComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    const subject = new Subject();
+    const subject = new BehaviorSubject(0);
 
     const series$ = subject.asObservable();
 
-    series$.subscribe(console.log);
+    series$.subscribe((val) => console.log('early sub:' + val));
 
     subject.next(1);
     subject.next(2);
     subject.next(3);
-    subject.complete();
+
+    // subject.complete();
+
+    setTimeout(() => {
+      series$.subscribe((val) => console.log('late sub:' + val));
+      subject.next(4);
+    }, 3000);
   }
 }
