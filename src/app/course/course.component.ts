1508ca6235d250c289b539688dc12e3acbdfe20c
syuancao <73942181+syuancao@users.noreply.github.com>
2022-05-30 17:50:16 +0800

    The startWith RxJs Operator - Simplifying the Course Component

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -8,6 +8,7 @@ import {
   fromEvent,
   map,
   Observable,
+  startWith,
   switchMap,
 } from 'rxjs';
 import { Lesson } from '../model/type';
@@ -35,15 +36,13 @@ export class CourseComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    const searchLessons$ = fromEvent<any>(this.input?.nativeElement, 'keyup').pipe(
+    this.lessons$ = fromEvent<any>(this.input?.nativeElement, 'keyup').pipe(
       map((event) => event.target.value),
+      startWith(''),
       debounceTime(400),
       distinctUntilChanged(),
       switchMap((search) => this.loadLessons(search)),
     );
-
-    const initialLessons$ = this.loadLessons();
-    this.lessons$ = concat(initialLessons$, searchLessons$);
   }
 
   loadLessons(search = ''): Observable<Lesson[]> {
